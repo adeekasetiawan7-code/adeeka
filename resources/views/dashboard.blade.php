580bd31294382a182bd52c8a71e2fc062c3d6450
adeeka <adeekasetiawan7@gmail.com>
2026-03-10 14:51:22 +0700

    login saja

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h1>Dashboard</h1>
+
+<p>dashboard saya</p>
+<br>
+
+<a href="/login">Logout</a>
+
+</body>
+</html>
